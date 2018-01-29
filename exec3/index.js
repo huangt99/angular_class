9c2c05e4496046b1f27a2408e6f83192b9c4520d
huangt99 <huangt99@yahoo.com>
2018-01-29 11:52:40 -0500

    added exec3

---
+++ exec3/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Working here')
+  })
+
+  
+app.get('/hello', function (req, res) {
+  res.send('Hello World!')
+})
+
+
+app.use('/static', express.static('static'))
+app.use('/test', express.static('public'))
+app.listen(2000, function () {
+  console.log('Test app listening on port 2000!')
+})
+
+
+// post function here
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/profile', upload.array(), function (req, res, next) {
+  console.log("Test Page");
+  console.log(req.body);
+  res.json(req.body);
+});
